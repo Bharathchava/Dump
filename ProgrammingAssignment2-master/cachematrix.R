b01862c820ba821fe8a026e0ab7f7cc2ad3564c2
Bharathchava <chava.bharath@gmail.com>
2018-08-28 15:59:05 +0530

    My first assignment commit

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,33 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(i) m <<- solve(x)
+  getInverse <- function() m
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  m <- x$getInverse()
+  if(!is.null(m))
+  {
+    return(m)
+  }
+  m <- solve(x$get())
+  x$setInverse(m)
+  m
+        ## Return a matrix that is the inverse of 'x'
+}
